8f3f631cec48592713edfc678a1f322052d74c9a
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-25 12:12:05 -0300

    style: login templates implemented

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { React } from "react";
-import Header from "./Header";
-import Footer from "./Footer";
+import Header from "./general/Header";
+import Footer from "./general/Footer";
 
 function Home() {
 
--- frontend/src/components/general/App.jsx
+++ frontend/src/components/general/App.jsx
@@ -4,7 +4,9 @@ import {
     Routes,
     Route,
   } from "react-router-dom";
-import Home from "./Home";
+import Home from "../Home";
+import SignIn from "../login/Sign-in";
+import SignUp from "../login/Sign-up";
 
 function App() {
   return (
@@ -13,6 +15,10 @@ function App() {
         <Routes>
 
           <Route path="/" element={<Home/>}>
+          </Route>
+          <Route path="/signin" element={<SignIn/>}>
+          </Route>
+          <Route path="/signup" element={<SignUp/>}>
           </Route>
           
         </Routes>
