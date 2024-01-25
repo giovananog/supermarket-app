4fe543a596cb248fa57de156ddde196aa1ac73fb
Giovana Nogueira <ongiovana2016@gmail.com>
2024-01-25 10:35:19 -0300

    init: initial app structure done

---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,15 @@
+import { React } from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+function Home() {
+
+  return (
+    <div className="home-div">
+      <Header />
+      <Footer />
+    </div>
+  );
+}
+
+export default Home;
